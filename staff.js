bd313a4857fbe322d0f1c5aed977d720f1d48f1a
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-01-30 10:41:05 -0500

    Started inplementing vue, added Mrs. Andreacci photo to folder

--- staff.js
+++ staff.js
@@ -2,9 +2,14 @@ const vue_app = Vue.createApp({
     // This automatically imports your movies.json file and puts it into
     //   the variable: movies
     created() {
-          fetch('staff.json').then(response => response.json()).then(staff => {
+          fetch('staff.json').then(response => response.json()).then(json => {
                 this.staff = json
           })
+    },
+    data() {
+      return {
+            staff: []
+      }
     },
     methods: {
           
