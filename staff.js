f02c833bb1216e15c3d58521820f24bd81fcccb7
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-01-31 11:54:39 -0500

    Changed scroll bar

--- staff.js
+++ staff.js
@@ -1,19 +1,19 @@
 const vue_app = Vue.createApp({
-    // This automatically imports your movies.json file and puts it into
-    //   the variable: movies
-    created() {
-          fetch('staff.json').then(response => response.json()).then(json => {
-                this.staff = json
-          })
-    },
-    data() {
-      return {
-            staff: []
+      // This automatically imports your movies.json file and puts it into
+      //   the variable: movies
+      created() {
+            fetch('staff.json').then(response => response.json()).then(json => {
+                  this.staff = json
+            })
+      },
+      data() {
+            return {
+                  staff: []
+            }
+      },
+      methods: {
+
       }
-    },
-    methods: {
-          
-    }
 })
 
 vue_app.mount("#vue_app")
