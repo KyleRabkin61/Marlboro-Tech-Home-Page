c489442d843a593f7ed888649a92f55a3000308e
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-02-04 22:36:54 -0500

    Implemented jQuery on the courses and clubs page. Clubs data is now being shown on the page, and the buttons are now functional and change from courses to clubs and vice versa.

--- courseandclub.js
+++ courseandclub.js
@@ -1,21 +1,35 @@
 const vue_app = Vue.createApp({
-      data() {
-          return {
-              courseandclub: {
+    data() {
+        return {
+            courseandclub: {
                 courses: [],
                 clubs: []
-              }
-          };
-      },
-      created() {
-          fetch('courseandclub.json')
-              .then(response => response.json())
-              .then(json => {
-                  this.courseandclub = json;
-              })
-              .catch(error => console.error("Error fetching courses:", error));
-      }
-  });
-  
-  vue_app.mount("#vue_app");
-  
+            }
+        };
+    },
+    created() {
+        fetch('courseandclub.json')
+            .then(response => response.json())
+            .then(json => {
+                this.courseandclub = json;
+            })
+            .catch(error => console.error("Error fetching data:", error));
+    }
+});
+
+vue_app.mount("#vue_app");
+
+$('document').ready(() => {
+
+    $('#coursesBtn').on('click', function () {
+        $('#theCourses').removeClass('d-none')
+        $('#theClubs').addClass('d-none')
+    })
+
+    $('#clubsBtn').on('click', function () {
+        $('#theClubs').removeClass('d-none')
+        $('#theCourses').addClass('d-none')
+    })
+
+})
+
