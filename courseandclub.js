a5f06dd58c67504a6e9f91d75744b97a349cff3e
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-02-09 13:13:14 -0500

    Made all pages have just the header only showcased on page load up, scroll function works with buttons on courses page, did some extra styling.

--- courseandclub.js
+++ courseandclub.js
@@ -24,11 +24,25 @@ $('document').ready(() => {
     $('#coursesBtn').on('click', function () {
         $('#theCourses').removeClass('d-none')
         $('#theClubs').addClass('d-none')
+        scrollToSection("theCourses");
     })
 
     $('#clubsBtn').on('click', function () {
         $('#theClubs').removeClass('d-none')
         $('#theCourses').addClass('d-none')
+        scrollToSection("theClubs");
     })
 
 })
+
+// button on scroll function that does not cut off div
+
+function scrollToSection(id) {
+    const element = document.getElementById(id);
+    const offset = 200; // Adjust this value based on navbar height
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+        top: elementPosition - offset,
+        behavior: "smooth"
+    });
+}
